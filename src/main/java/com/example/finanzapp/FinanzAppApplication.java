c48670eb60a5847cea47e5a7f08c93cf367b32e2
Alexander20041 <aforeroc5@ucentral.edu.co>
2025-05-08 23:23:24 -0500

    Subida de prueba

--- src/main/java/com/example/finanzapp/FinanzAppApplication.java
+++ src/main/java/com/example/finanzapp/FinanzAppApplication.java
@@ -1,5 +1,6 @@
 package com.example.finanzapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinanzAppApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(FinanzAppApplication.class, args);
 	}
 }
